d528f51f765d87d2f4d460cc0683a9bb77876d71
Joe Zusi <joe.zusi@ticketnetwork.com>
2019-12-13 19:04:43 +0000

    Reinitialized without database

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,3 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/start', to: 'application#start'
-  post '/start', to: 'application#start'
-
-  get '/suits', to: 'application#suits'
-  post '/suits', to: 'application#suits'
-
-  get '/card', to: 'application#card'
-  post '/card', to: 'application#card'
-
-  get '/name', to: 'application#name'
-  post '/name', to: 'application#name'
-
-  get '/download', to: 'application#download'
-
-  root 'application#start'
 end
