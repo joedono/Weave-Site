98aa45890b224ce37ca0664e9435f1e08ecbbd1b
Joe Zusi <joe.zusi@ticketnetwork.com>
2019-12-13 17:08:07 +0000

    Added routes and view stubs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # Playset Selection
+  def start
+  end
+
+  # Core Suit and Level
+  def suits
+  end
+
+  # Cards for selecting qualities
+  def card
+  end
+
+  # Character Name
+  def name
+  end
+
+  # Print/Download Character Sheet
+  def download
+  end
 end
