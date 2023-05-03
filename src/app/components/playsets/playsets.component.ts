d6d73ae770d0600a5ff60334523fe7b8529b6612
joedono <fjzusi@gmail.com>
2023-05-03 16:09:33 -0400

    WIP playsets component

--- src/app/components/playsets/playsets.component.ts
+++ src/app/components/playsets/playsets.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PlaysetMetaModel } from 'src/app/models/playset-meta.model';
+import { PlaysetsService } from 'src/app/services/playsets.service';
 
 @Component({
   selector: 'app-playsets',
   templateUrl: './playsets.component.html',
   styleUrls: ['./playsets.component.less']
 })
-export class PlaysetsComponent {
+export class PlaysetsComponent implements OnInit {
+
+  playsets: PlaysetMetaModel[] = [];
+  selectedPlayset: PlaysetMetaModel | undefined;
+
+  constructor(private playsetsService: PlaysetsService) { }
+
+  ngOnInit(): void {
+    this.playsetsService.getPlaysets()
+      .subscribe(playsets => this.playsets = playsets);
+  }
+
+  onPlaysetSelect(playset: PlaysetMetaModel) {
+    this.selectedPlayset = playset;
+    console.log(this.selectedPlayset);
+  }
 
 }
--- src/app/services/playsets.service.ts
+++ src/app/services/playsets.service.ts
@@ -1,9 +1,43 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, map, of, tap } from 'rxjs';
+import { parse } from 'yaml';
+import { PlaysetMetaModel } from '../models/playset-meta.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlaysetsService {
 
-  constructor() { }
+  private playsetsUrl = './assets/playsets.yml';
+  private playsets: PlaysetMetaModel[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  getPlaysets(): Observable<PlaysetMetaModel[]> {
+    if (this.playsets.length > 0) {
+      return of(this.playsets);
+    }
+
+    return this.http.get(this.playsetsUrl, {
+      observe: 'body',
+      responseType: 'text'
+    }).pipe(
+      map(yamlString => parse(yamlString)),
+      map(yaml => this.convertPlaysets(yaml))
+    );
+  }
+
+  private convertPlaysets(yaml: any): PlaysetMetaModel[] {
+    this.playsets = [];
+    yaml.forEach((element: PlaysetMetaModel) => {
+      element.file = 'assets/' + element.folder + '/' + element.file;
+      element.img = 'assets/' + element.folder + '/' + element.img;
+      element.glossary = 'assets/' + element.folder + '/' + element.glossary;
+
+      this.playsets.push(element);
+    });
+
+    return this.playsets;
+  }
 }
