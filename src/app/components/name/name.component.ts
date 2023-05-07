d7731bba699d02c4d241bc3b190cb1b4f3947916
joedono <fjzusi@gmail.com>
2023-05-07 00:40:02 -0400

    Completed Name component

--- src/app/components/name/name.component.ts
+++ src/app/components/name/name.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { BuilderService } from 'src/app/services/builder.service';
 
 @Component({
   selector: 'app-name',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./name.component.less']
 })
 export class NameComponent {
+
+  name: string = '';
+
+  constructor(
+    private router: Router,
+    private builderService: BuilderService
+  ) { }
+
+  continue(): void {
+    this.builderService.setName(this.name);
+    let queryParams = this.builderService.getCharacterQueryString();
+
+    this.router.navigate(['character-sheet'], { queryParams: queryParams });
+  }
+
+  reset(): void {
+    this.builderService.reset();
+    this.router.navigate(['']);
+  }
 
 }
