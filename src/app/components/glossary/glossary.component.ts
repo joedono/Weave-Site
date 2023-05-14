f4b9ad62acca9aca25bfa83c918c87c342baa8d8
joedono <fjzusi@gmail.com>
2023-05-14 17:33:08 -0400

    Added ngOnInot to glossary component

--- src/app/components/glossary/glossary.component.ts
+++ src/app/components/glossary/glossary.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PlaysetsService } from 'src/app/services/playsets.service';
 
 @Component({
   selector: 'app-glossary',
   templateUrl: './glossary.component.html',
   styleUrls: ['./glossary.component.less']
 })
-export class GlossaryComponent {
+export class GlossaryComponent implements OnInit {
+
+  playsetId: string = '';
+  playsetPath: string = '';
+
+  constructor(
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      this.playsetId = params['playset'];
+      this.playsetPath = './assets/' + this.playsetId + '/' + this.playsetId + '.md';
+    });
+  }
 
 }
