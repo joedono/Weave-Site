fc25baff015b48f7c9462630789b51c80a0ad460
joedono <fjzusi@gmail.com>
2023-05-16 16:24:07 -0400

    Implemented Import Character

--- src/app/components/import-character/import-character.component.ts
+++ src/app/components/import-character/import-character.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { BuilderService } from 'src/app/services/builder.service';
 
 @Component({
   selector: 'app-import-character',
@@ -6,5 +8,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./import-character.component.less']
 })
 export class ImportCharacterComponent {
+
+  name: string = '';
+  suit: string = '';
+  level: number = 1;
+  characterConfigs: string = '';
+
+  constructor(
+    private router: Router,
+    private builderService: BuilderService
+  ) { }
+
+  selectedSuitEvent(suit: string): void {
+    this.suit = suit;
+  }
+
+  continue(): void {
+    if (this.name == ''){
+      alert('You need a Name');
+    } else if (this.level <= 0 || this.level > 10) {
+      alert('Starting Level must be between 1 and 10');
+    } else if (this.suit == '') {
+      alert('You must select a Core Suit');
+    } else if (this.characterConfigs == '') {
+      alert('You must import character config');
+    } else {
+      this.builderService.setName(this.name);
+      this.builderService.setCoreSuit(this.suit);
+      this.builderService.setCurrentLevel(this.level);
+
+      let characterConfigs = this.characterConfigs.split('\n').filter(str => str);
+      let queryParams = this.builderService.getCharacterQueryString();
+      queryParams['character'] = characterConfigs;
+
+      this.router.navigate(['character-sheet'], { queryParams: queryParams });
+    }
+  }
+
+  reset(): void {
+    this.builderService.reset();
+    this.router.navigate(['']);
+  }
 
 }
